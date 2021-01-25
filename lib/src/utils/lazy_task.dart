b07094502ef476ba8f585907a6df436c4ca0d6b7
arish <arishsultan104@gmail.com>
2021-01-25 17:00:31 +0500

    :heavy_plus_sign: Added utilities

---
+++ lib/src/utils/lazy_task.dart
@@ -0,0 +1,23 @@
+import 'package:agent_pet/src/widgets/dialogs/loading-dialog.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:pedantic/pedantic.dart';
+
+
+Future<dynamic> performLazyTask(
+  BuildContext context,
+  Future<dynamic> Function() task, {
+  String message = 'Please Wait',
+  bool persistent = true,
+}) async {
+  if (persistent) {
+    unawaited(openLoadingDialog(context, message));
+  } else {
+    unawaited(openLoadingDialog(context, message));
+  }
+
+  final result = await task();
+  Navigator.of(context).pop();
+
+  return result;
+}
